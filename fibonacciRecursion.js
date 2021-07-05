a896680e0044fae8a451425a107d0f1f656f4787
Jimmie Smith <84278458+FriendOfDorothy@users.noreply.github.com>
2021-07-05 13:30:23 -0400

    Update fibonacciRecursion.js

--- fibonacciRecursion.js
+++ fibonacciRecursion.js
@@ -7,8 +7,50 @@ The logic was difficult to wrap my head
 around at first, but became easier when I tracked my values.
 The function below works as follows:
 
-say that the input was "fibber(4)"
+say that the input was "fibber(4)" the data structure
+would look like this:
 
+
+                      fibber(4) = 3
+
+        Meaning the 4th value in the fibonacci sequence is 3
+
+               As logged by the console :)
+
+
+                       fibber(4)
+
+           Value = 2    /    add   \  Value = 1
+
+                      /              \  
+
+              fibber(3)    +    fibber(2)
+
+  Value = 1    / add \  Value = 1        \
+                                       
+             /         \                   \
+
+          fibber(2)  +  fibber(1)        /   \
+   
+Value 1 / add \ Value = 0 Value = 1    /  add  \ Value = 0
+                                        
+      /          \                   /           \
+  fibber(1) fibber(0)        fibber(1)    +    fibber(0)
+
+
+fibber(1) = 1 so we return 1.
+fibber(0) = 0 so we return 0.
+we then add those elements to get the
+value of fibber(2), which is 0 + 1 = 1
+The base conditions have been met
+so we can move on. The function must first
+create a value for the left side (n-1) before
+a value is created for the right side (n-2).
+The tree goes down the left side until a base
+condition is met (fibber(1)) and then traverses 
+down the right side until a base condition is met.
+then it returns the values specified in the return statement
+until it reaches the top again!
 */
 
   
@@ -19,13 +61,30 @@ say that the input was "fibber(4)"
   
 }
 
+/*
+Creating a function to log the values to the console
+was probably the hardest part of this. I sat for hours
+thinking how I could iterate through an array of numbers
+without an iterative loop. Then it hit me, I could use
+another recursive function with an argument to specify
+the number of values to log dynamically as well as 
+pass an argument to the function 'fibber' to iterate
+through the fuction itself rather than through an array.
+*/
+
+
  function fibCount(headCount){
   if(headCount === 0) return;
   fibCount(headCount-1);
-   fibIndex = headCount;
-  console.log(fibber(fibIndex));
+  console.log(fibber(headcount));
 } 
 
+
+/*
+so here, i just used a simple recursive functiion to 
+return numbers 1 - headCount, which is expressed as integer
+below
+*/
 fibCount(15);
 
 
